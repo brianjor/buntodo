49d40b1581f4efa920ab83d12561d54edd705dd7
brianjor <22751140+brianjor@users.noreply.github.com>
2023-08-20 02:07:35 +0000

    Back to Vite

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,11 +1,14 @@
 import { Elysia } from "elysia";
 import Router from "./router.ts";
 
+const port = 8080;
 const app = new Elysia();
 
 Router.route(app);
 
-app.listen(8080);
+app.listen(port, () => {
+	console.log(`Started server on http://localhost:${port}`);
+});
 
 export type App = typeof app;
 
---
+++ packages/ui/happydom.ts
@@ -0,0 +1,4 @@
+import { GlobalRegistrator } from "@happy-dom/global-registrator";
+
+console.log("Registering happy-dom GlobalRegistrator");
+GlobalRegistrator.register();
