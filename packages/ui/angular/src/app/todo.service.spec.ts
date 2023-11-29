e4c6edf76630ea10726c4a51925788bf9cab850b
brianjor <22751140+brianjor@users.noreply.github.com>
2023-11-28 21:36:04 -0800

    Fixed basic angular tests not passing

--- packages/ui/angular/src/app/todo.service.spec.ts
+++ packages/ui/angular/src/app/todo.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TodoService } from './todo.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('TodoServiceService', () => {
 	let service: TodoService;
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({});
+		TestBed.configureTestingModule({
+			imports: [HttpClientModule],
+		});
 		service = TestBed.inject(TodoService);
 	});
 
